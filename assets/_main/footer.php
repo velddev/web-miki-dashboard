a36bfc300e72e752d268c2f0e659c39c88ed25f0
AlexFlipnote <alexander.samuelsen.manso@gmail.com>
2017-01-06 19:22:30 +0100

    A complete remake

---
+++ assets/_main/footer.php
@@ -0,0 +1,3 @@
+<footer>
+  v0.1 | Report bugs to <a href="https://github.com/velddev/web-miki-patreon/issues" target="_blank">GitHub<i class="fa fa-github"></i></a>
+</footer>
---
+++ assets/_main/head.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<link rel="stylesheet" href="assets/css/font-awesome.min.css">
